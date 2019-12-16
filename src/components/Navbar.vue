c2fdddfeec6874a9a4fd89b520f2544c8c0f343e
Carlos Wimmer <carloswimmer@santos.sp.gov.br>
2019-12-16 18:08:56 -0300

    Complete navigation drawer

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -12,13 +12,36 @@
           alt="Logo Prefeitura de Santos"
           class="shrink ml-3"
           contain
-          src="../assets/tribus.png"
+          src="../assets/centralservidor-logo-texto.png"
           transition="scale-transition"
-          width="150"
+          width="140"
         />
       </div>
 
       <v-spacer></v-spacer>
+
+      <div class="text-center">
+        <v-menu offset-y>
+          <template v-slot:activator="{ on }">
+            <v-btn text dark v-on="on">
+              <v-icon left>mdi-chevron-down</v-icon>
+              Links Úteis
+            </v-btn>
+          </template>
+          <v-list class="accent--text">
+            <v-list-item 
+              v-for="(link, index) in links" 
+              :key="index" 
+              router 
+              :to="link.route"
+            >
+              <v-list-item-title class="accent--text">
+                {{ link.title }}
+              </v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+      </div>
 
       <v-btn
         href="https://github.com/vuetifyjs/vuetify/releases/latest"
@@ -31,13 +54,25 @@
     </v-app-bar>
 
     <v-navigation-drawer 
-      dark 
+      dark
       clipped 
       app 
       v-model="drawer" 
-      color="primary darken-2">
+      color="primary lighten-3">
       <v-list>
-        
+        <v-list-item>
+          <v-list-item-avatar class="accent">
+            <v-span class="primary--text text--darken-2">CW</v-span> 
+          </v-list-item-avatar>
+        </v-list-item>
+
+        <v-list-item class="mb-4">
+          <v-list-item-content>
+            <v-list-item-title class="title">Carlos Wimmer</v-list-item-title>
+            <v-list-item-subtitle>Reg. 30.867-6</v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+
         <v-list-item v-for="item in items" :key="item.title" router :to="item.route">
           <v-list-item-icon>
             <v-icon>{{ item.icon }}</v-icon>
@@ -58,9 +93,19 @@ export default {
     return {
       drawer: true,
       items: [
-        { title: 'Dashboard', icon: 'mdi-view-dashboard', route: '/' },
-        { title: 'My Projects', icon: 'mdi-folder', route: '/projects' },
-        { title: 'Team', icon: 'mdi-account', route: '/team' }
+        { title: 'Declarações', icon: 'mdi-home-currency-usd', route: '/dec' },
+        { title: 'Avaliações', icon: 'mdi-file-document-edit-outline', route: '/ava' },
+        { title: 'Consignados', icon: 'mdi-bank', route: '/consig' },
+        { title: 'Recadastramento', icon: 'mdi-account-card-details-outline', route: '/recad' },
+        { title: 'Formulários', icon: 'mdi-ballot-outline', route: '/form' },
+      ],
+      links: [
+        { title: 'WebMail', route: '/dec' },
+        { title: 'Perícia Médica', route: '/ava' },
+        { title: 'Holerite', route: '/consig' },
+        { title: 'Contagem de Tempo', route: '/recad' },
+        { title: 'Estatuto', route: '/form' },
+        { title: 'Portal do Servidor', route: '/port' },
       ]
     }
   }
